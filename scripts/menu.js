ac8ddc5f39f56077f47205e9227ecc5a3d7a815e
Alfredo Sierra <developerfullstack98@gmail.com>
2023-11-29 19:57:03 -0500

    acondicionamiento del boton stock de la vista menu

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,3 +2,10 @@ function verNombre(name){
     document.getElementById('user').innerHTML= "Bienvenido," + name + " " + "a tu inventario!"
 }
 verNombre(localStorage.getItem("user"));
+
+
+// Llama la pagina del inventario
+let botonStock = document.getElementById("stock");
+botonStock.addEventListener("click", function() {
+    window.location.href = "admin.html";
+});
